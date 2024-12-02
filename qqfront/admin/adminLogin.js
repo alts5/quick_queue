b40f9c46f2800603b88fa561facca7e6fdeb208f
alts5 <133884186+alts5@users.noreply.github.com>
2024-12-02 18:34:26 +0300

    Добавил связь страницы входа с бэком

---
+++ qqfront/admin/adminLogin.js
@@ -0,0 +1,28 @@
+var pathToBackend = window.location.hostname;
+
+
+$('#auth_form').on('submit', function(e) {
+	e.preventDefault();
+	$.ajax({
+		url: 'http://' + pathToBackend + ':8080/authenticate',     
+		method: 'POST',
+		dataType: 'json',
+		contentType: "application/x-www-form-urlencoded",
+		data: $(this).serialize(),
+		success: function(data){
+			$('.error_base').css('display','none');
+			sessionStorage.setItem('token', data["token"]);
+			window.location.href="/workroom.html";
+		},
+		error: function(data){
+			var error = data["error"] || "Произошла ошибка при обращении к API";
+			$('.error_base').text(error);
+			$('.error_base').css('display','block');
+		}
+	});
+	return false;
+});
+
+function getUser() {
+	return false;
+}
